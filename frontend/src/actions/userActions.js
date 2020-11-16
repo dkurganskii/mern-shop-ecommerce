eb3fb92c7b2d0525231255bc617dac04128296fd
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-11-16 16:56:54 -0600

    implement admin user list

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -13,7 +13,10 @@ import {
     USER_REGISTER_SUCCESS,
     USER_UPDATE_PROFILE_FAIL,
     USER_UPDATE_PROFILE_REQUEST,
-    USER_UPDATE_PROFILE_SUCCESS
+    USER_UPDATE_PROFILE_SUCCESS,
+    USER_LIST_REQUEST,
+    USER_LIST_SUCCESS,
+    USER_LIST_FAIL
 } from "../constants/userConstants"
 import { ORDER_LIST_MY_RESET } from '../constants/orderConstants'
 
@@ -57,20 +60,16 @@ export const register = (name, email, password) => async (dispatch) => {
         dispatch({
             type: USER_REGISTER_REQUEST
         })
-
         const config = {
             headers: {
                 'Content-Type': 'application/json'
             }
         }
-
         const { data } = await axios.post('/api/users', { name, email, password }, config)
-
         dispatch({
             type: USER_REGISTER_SUCCESS,
             payload: data
         })
-
         dispatch({
             type: USER_LOGIN_SUCCESS,
             payload: data
@@ -148,6 +147,39 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
     } catch (error) {
         dispatch({
             type: USER_UPDATE_PROFILE_FAIL,
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
+        })
+    }
+}
+
+export const listUsers = () => async (dispatch, getState) => {
+    try {
+        dispatch({
+            type: USER_LIST_REQUEST,
+        })
+
+        const {
+            userLogin: { userInfo },
+        } = getState()
+
+        const config = {
+            headers: {
+                Authorization: `Bearer ${userInfo.token}`,
+            },
+        }
+
+        const { data } = await axios.get(`/api/users`, config)
+
+        dispatch({
+            type: USER_LIST_SUCCESS,
+            payload: data,
+        })
+    } catch (error) {
+        dispatch({
+            type: USER_LIST_FAIL,
             payload:
                 error.response && error.response.data.message
                     ? error.response.data.message
