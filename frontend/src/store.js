c5df8c4ec3e28d79a0e56c53e19ae66be4655ad9
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-11-17 14:12:34 -0600

    add update user functionality

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -9,7 +9,8 @@ import {
     userDetailsReducer,
     userUpdateProfileReducer,
     userListReducer,
-    userDeleteReducer
+    userDeleteReducer,
+    userUpdateReducer
 } from './reducers/userReducers'
 import {
     orderCreateReducer,
@@ -28,10 +29,12 @@ const reducer = combineReducers({
     userUpdateProfile: userUpdateProfileReducer,
     userList: userListReducer,
     userDelete: userDeleteReducer,
+    userUpdate: userUpdateReducer,
     orderCreate: orderCreateReducer,
     orderDetails: orderDetailsReducer,
     orderPay: orderPayReducer,
     orderListMy: orderListMyReducer,
+
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? JSON.parse
