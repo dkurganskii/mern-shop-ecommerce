0c1df223ce17d5c9f8cc198f5526d4a0e313c91c
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-11-22 08:57:38 -0600

    implement frontend image upload

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -7,15 +7,19 @@ const storage = multer.diskStorage({
     destination(req, file, cb) {
         cb(null, 'uploads/')
     },
-    fileName(req, file, cb) {
-        cb(null, `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`)
-    }
+    filename(req, file, cb) {
+        cb(
+            null,
+            `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
+        )
+    },
 })
 
 function checkFileType(file, cb) {
     const filetypes = /jpg|jpeg|png/
     const extname = filetypes.test(path.extname(file.originalname).toLowerCase())
     const mimetype = filetypes.test(file.mimetype)
+
     if (extname && mimetype) {
         return cb(null, true)
     } else {
@@ -27,7 +31,7 @@ const upload = multer({
     storage,
     fileFilter: function (req, file, cb) {
         checkFileType(file, cb)
-    }
+    },
 })
 
 router.post('/', upload.single('image'), (req, res) => {
