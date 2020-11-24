4d8a8427805d633b591157731e896c052dc84d6b
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-11-24 06:59:21 -0600

    fix styles

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -91,7 +91,7 @@ const OrderScreen = ({ match, history }) => {
         <Message variant='danger'>{error}</Message>
     ) : (
                 <>
-                    <h1>Order {order._id}</h1>
+                    <h4>Order {order._id}</h4>
                     <Row>
                         <Col md={8}>
                             <ListGroup variant='flush'>
